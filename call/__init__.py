ea60ac84ad6672b56b798413ce66a4c88180df97
lxl66566 <18259734087@163.com>
2023-01-18 11:12:45 +0800

    bug remains

--- call/__init__.py
+++ call/__init__.py
@@ -1,11 +1,10 @@
 from nonebot import on_keyword,on_message
 from nonebot.rule import to_me
-from nonebot.params import CommandArg, Message
 from nonebot.adapters.onebot.v11.message import MessageSegment
 from nonebot.adapters.onebot.v11 import GROUP
 from .data import atri_text
 from .data import V_PATH
-import re,random
+import random
 
 call = on_keyword(['亚托莉','アトリ','atri'], priority=1, block=True)
 call2 = on_message(rule=to_me(),permission=GROUP, priority=1)
@@ -15,9 +14,11 @@ async def _():
     await call.finish('haihai~~~~(≧▽≦)')
 
 @call2.handle()
-async def _2():
+async def _():
     voice = random.choice(atri_text)["o"]
-    text = re.findall("(.*).mp3", voice)[0]
-    await call2.send(MessageSegment.record(f"file:///{V_PATH}{voice}"))
+    # text = re.findall("(.*).mp3", voice)[0]
+    text = voice.rstrip(".mp3")
+    print(f"voice = {voice},text = {text}")
+    await call2.send(MessageSegment.record(f"file:///{V_PATH}" + str(voice)))
     await call2.finish(text)
     # await call.finish('haihai~~~~(≧▽≦)')
